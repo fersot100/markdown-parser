c49472d5f9df9488c48eccca2b583bd40532c460
Unknown <magicdustin14@gmail.com>
2017-12-11 17:44:06 -0800

    organized root directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import ScrollView from './components/scroll-view';
 import Comments from './components/comments';
 
