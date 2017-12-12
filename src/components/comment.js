a8a8690b7fce739572c635e60add78a8844cd6f6
Unknown <magicdustin14@gmail.com>
2017-12-11 19:41:44 -0800

    Added like and reply button to comments

--- src/components/comment.js
+++ src/components/comment.js
@@ -7,6 +7,10 @@ export const comment = ({title, body, student}) => (
         <p className='comment__student-school'>{student.school}</p>
       </div>
         <p>{body}</p>
+        <div className='comment__button'>
+          <button className='button button--reply '>Reply</button>
+          <button className='button button--reply '>Like</button>
+        </div>
   </div>
 );
 
